a5da1b014a3f5578ef49d5384bfa18d7cfe2549f
Richard Cummings <richardecummings@gmail.com>
2019-02-18 17:31:48 -0500

    init

---
+++ ContactsApp.Android/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ContactsApp.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Contact))]
+namespace ContactsApp.Droid
+{
+    public class Contact : IContact
+    {
+        public Contact()
+        {
+        }
+
+        public string ByeContact()
+        {
+            Contacts.Contact contact = new Contacts.Contact("Richard", "Cummings", "ysl@example.com");
+            return contact.ByeContact();
+        }
+
+        public string HelloContact()
+        {
+            Contacts.Contact contact = new Contacts.Contact("Richard", "Cummings", "ysl@example.com");
+            return contact.GreetContact();
+        }
+    }
+}
