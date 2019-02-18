f591d867a2863dde0c11af60490a4efff2ac9c07
Richard Cummings <richardecummings@gmail.com>
2019-02-18 17:48:52 -0500

    Linking for APK sizes

--- ContactsApp.Android/Contact.cs
+++ ContactsApp.Android/Contact.cs
@@ -5,10 +5,13 @@ using Xamarin.Forms;
 [assembly: Dependency(typeof(Contact))]
 namespace ContactsApp.Droid
 {
+    [Android.Runtime.Preserve (AllMembers = false)]
     public class Contact : IContact
     {
+        [Android.Runtime.Preserve(Conditional = false)]
         public Contact()
         {
+            // code that I want to preserve
         }
 
         public string ByeContact()
