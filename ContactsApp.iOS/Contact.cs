71904cbe5ce2d04865e6fc6f328f4dc59e41f3bc
Richard Cummings <richardecummings@gmail.com>
2019-02-19 19:16:28 -0500

    Building ObjectiveC libraries

---
+++ ContactsApp.iOS/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ContactsApp.iOS;
+using Xamarin.Forms;
+using Foundation;
+
+[assembly: Dependency(typeof(Contact))]
+namespace ContactsApp.iOS
+{
+    public class Contact : IContact
+    {
+        public Contact()
+        {
+        }
+
+        public string ByeContact()
+        {
+            ContactsLibraryiOS.ContactsApp contact = new ContactsLibraryiOS.ContactsApp((NSString)"Richard", (NSString)"Cummings", (NSString)"ysl@example.com");
+            return contact.ByeContact();
+        }
+
+        public string HelloContact()
+        {
+            ContactsLibraryiOS.ContactsApp contact = new ContactsLibraryiOS.ContactsApp((NSString)"Richard", (NSString)"Cummings", (NSString)"ysl@example.com");
+            return contact.GreetContact();
+        }
+    }
+}
